8cf7c670dc1f6c4b935c26c9738e3ab9d6cf0004
tampambro <tampambro999@gmail.com>
2020-04-03 15:39:56 +0300

    Added upgrade. 1 - 'Navigator.geolocation' is removed and  replaced by IP request (iIt's implemented through additional APIs: [https://www.ipify.org/], [https://api.sypexgeo.net/json/] ). Thanks to this, the user does not need to confirm his geolocation. 2 - Preloader added.

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
 `use strict`;
 
-window.addEventListener(`load`, () => {
-    let long;
-    let lat;
+window.addEventListener(`load`, async () => {
     let locationTimezone = document.querySelector(`.location-timezone`);
     let temperatureDegree = document.querySelector(`.temperature-degree`);
     let temperatureDescription = document.querySelector(`.temperature-description`);
     const temperatureDiv = document.querySelector(`.temperature`);
     let temperatureKind = document.querySelector(`.temperature span`);
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+    const proxy = `https://cors-anywhere.herokuapp.com/`
 
-            const proxy = `https://cors-anywhere.herokuapp.com/`
-            const weatherApi = `${proxy}https://api.darksky.net/forecast/dd294ab06f407d7583d852afd45bb9c7/${lat},${long}`;
+    const jsonRequestIP = await fetch(`${proxy}https://api.ipify.org?format=json`);
+    const jsonIP = await jsonRequestIP.json();
+    const userIP = await jsonIP.ip;
 
-            fetch(weatherApi)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    const {temperature, summary, icon} = data.currently;
-                    let celsius = (temperature - 32) * (5 / 9);
+    const jsonRequestCoordinates = await fetch(`${proxy}https://api.sypexgeo.net/json/${userIP}`);
+    const jsonCoordinates = await jsonRequestCoordinates.json();
+    const latitude = await jsonCoordinates.city.lat;
+    const longitude = await jsonCoordinates.city.lon;
 
-                    temperatureDegree.textContent = Math.trunc(celsius * 10) / 10;
-                    temperatureDescription.innerHTML = `<b>${summary}</b>`;
-                    locationTimezone.textContent = data.timezone;
+    const weatherAPIRequest = await fetch(`${proxy}https://api.darksky.net/forecast/dd294ab06f407d7583d852afd45bb9c7/${latitude},${longitude}`);
+    const weatherAPIJson = await weatherAPIRequest.json();
 
-                    setIcons( icon, document.querySelector(`.icon`) );
 
-                    temperatureDiv.addEventListener(`click`, () => {
-                        if (temperatureKind.textContent === `\u00B0F`) {
-                            temperatureKind.textContent = `
-                            \u00B0C`;
-                            temperatureDegree.textContent = Math.trunc(celsius * 10) / 10;
-                        } else {
-                            temperatureKind.textContent = `\u00B0F`;
-                            temperatureDegree.textContent = temperature;
-                        }
-                    });
-                });
-        });
+    const {temperature, summary, icon} = weatherAPIJson.currently;
 
-        let now = new Date();
-        let hours = now.getHours();
+    let celsius = (temperature - 32) * (5 / 9);
+    temperatureDegree.textContent = Math.trunc(celsius * 10) / 10;
+    temperatureDescription.innerHTML = `<b>${summary}</b>`;
+    locationTimezone.textContent = weatherAPIJson.timezone;
 
-        if (hours > 6 && hours < 19) {
-            document.body.style.background = `linear-gradient(rgb(47, 150, 163), rgb(8, 67, 190))`
+    setIcons( icon, document.querySelector(`.icon`) );
+
+    temperatureDiv.addEventListener( `click`, () => {
+        if (temperatureKind.textContent === `\u00B0F`) {
+            temperatureKind.textContent = `
+            \u00B0C`;
+            temperatureDegree.textContent = Math.trunc(celsius * 10) / 10;
         } else {
-            document.body.style.background = `linear-gradient(rgb(5, 26, 189), rgb(5, 5, 84))`
-        };
-
-        function setIcons(icon, iconID) {
-            const skycons = new Skycons({color: `#ebebeb`});
-            const currentIcon = icon.replace(/-/g, `_`).toUpperCase();
-            skycons.play();
-            return skycons.set(iconID, Skycons[currentIcon]);
+            temperatureKind.textContent = `\u00B0F`;
+            temperatureDegree.textContent = temperature;
         }
+
+    } );
+
+    const preloader = document.getElementById(`page-preloader`);
+
+    if ( !preloader.classList.contains(`done`) ) {
+        preloader.classList.add(`done`);
+    }
+
+    let now = new Date();
+    let hours = now.getHours();
+
+    if (hours > 6 && hours < 19) {
+        document.body.style.background = `linear-gradient(rgb(47, 150, 163), rgb(8, 67, 190))`
+    } else {
+        document.body.style.background = `linear-gradient(rgb(5, 26, 189), rgb(5, 5, 84))`
     };
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: `#ebebeb`});
+        const currentIcon = icon.replace(/-/g, `_`).toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+
 });
-
-
-
 
